d57124e289bf92ef513760509ebdfb1d67db81d7
Nicteha Pinkus <nictehapinkus@gmail.com>
2021-05-18 00:04:05 -0700

    netlify form

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -2,34 +2,34 @@ import { useState } from 'react';
 import './Contact.scss';
 
 function Contact() {
-  const [status, setStatus] = useState("Submit");
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setStatus("Sending...");
-    const { name, email, message } = e.target.elements;
-    let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-    let response = await fetch("http://localhost:8080/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
-    });
-    setStatus("Submit");
-    let result = await response.json();
-    e.target.reset();
-    alert(result.status);
+  // const [status, setStatus] = useState("Submit");
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   setStatus("Sending...");
+  //   const { name, email, message } = e.target.elements;
+  //   let details = {
+  //     name: name.value,
+  //     email: email.value,
+  //     message: message.value,
+  //   };
+  //   let response = await fetch("http://localhost:8080/contact", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json;charset=utf-8",
+  //     },
+  //     body: JSON.stringify(details),
+  //   });
+  //   setStatus("Submit");
+  //   let result = await response.json();
+  //   e.target.reset();
+  //   alert(result.status);
     
-  };
+  // };
 
   return (
     <div className='contact'>
       <div className='contact__left'>
-        <form className='contact__form' onSubmit={handleSubmit}>
+        <form className='contact__form' methods='POST' data-netlify="true" >
           <div className='contact__container'>
             <input className='contact__input' type='text' name='name' placeholder={`What's your name?`} required></input>
             <input className='contact__input' type='email' name='email' placeholder='Email?' required></input>
