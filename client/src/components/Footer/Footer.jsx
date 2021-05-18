d03b23aab22d28d6f943dd29822bf8de84d95939
Nicteha Pinkus <nictehapinkus@gmail.com>
2021-05-18 14:43:24 -0700

    link rerouting

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -9,8 +9,8 @@ function Footer() {
   return (
     <div className='foot'>
       <a className='foot__link' target='_blank' rel='external' href='https://www.linkedin.com/in/nictehapinkus158/'><AiOutlineLinkedin/></a>
-      <a className='foot__link' target='_blank' rel='external' href='https://www.github.com/nickyiie'><AiOutlineInstagram/></a>
-      <a className='foot__link' target='_blank' rel='external' href='https://www.instagram.com/nictehahaha/'><AiOutlineGithub/></a>
+      <a className='foot__link' target='_blank' rel='external' href='https://www.instagram.com/nictehahaha/'><AiOutlineInstagram/></a>
+      <a className='foot__link' target='_blank' rel='external' href='https://www.github.com/nickyiie'><AiOutlineGithub/></a>
     </div>
   )
 }
